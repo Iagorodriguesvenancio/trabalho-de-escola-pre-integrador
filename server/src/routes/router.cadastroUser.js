f7666c43aa9fc371bce043d320a34dfbe40adda5
iago rodrigues venâncio <166940571+Iagorodriguesvenancio@users.noreply.github.com>
2026-07-30 11:13:50 -0300

    Delete server directory

--- server/src/routes/router.cadastroUser.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import cUser from "../controller/controller.cadastroUser.js" 
-
-const routeUser = express.Router()
-
-routeUser.get("/",cUser)
-
-export default routeUser 
--- server/src/routes/routes.login.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import login from "../controller/controller.login.js"
-
-const route = express.Router()
-
-route.get("/:email&senha",login)
-
-export default route 
